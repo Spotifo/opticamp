4f619a4464dadeffe708aa3eeb5c9a69ab180c16
Spotifo <lluisllobet26@gmail.com>
2025-06-14 14:06:28 +0200

    Add files via upload

--- api.py
+++ api.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
 from fastapi import FastAPI, File, UploadFile, HTTPException, Depends
 from fastapi.responses import JSONResponse, StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
